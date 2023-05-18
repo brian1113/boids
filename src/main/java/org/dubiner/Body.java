39bd747b5b27d6ec6fbabaffdadf2b2161d7a320
spacecat <library4lion@gmail.com>
2023-05-17 23:05:24 -0700

    did stuff

--- src/main/java/org/dubiner/Body.java
+++ src/main/java/org/dubiner/Body.java
@@ -1,2 +1,64 @@
-package org.dubiner;public class Body {
+package org.dubiner;
+
+public class Body {
+    private double m, x, y, vX, vY, aX, aY;
+    private final double pX, pY, pvX, pvY, paX, paY;
+    public Body(double m, double x, double y, double vX, double vY, double aX, double aY) {
+        this.m = m;
+        this.x = x;
+        this.y = y;
+        this.vX = vX;
+        this.vY = vY;
+        this.aX = aX;
+        this.aY = aY;
+
+        this.pX = x;
+        this.pY = y;
+        this.pvX = vX;
+        this.pvY = vY;
+        this.paX = aX;
+        this.paY = aY;
+    }
+
+    public void update(double fgx, double fgy) {
+        aX = fgx / m;
+        aY = fgy / m;
+        vX = vX + aX;
+        vY = vY + aY;
+        x += vX;
+        y += vY;
+    }
+
+    public void reset() {
+        x = pX;
+        y = pY;
+        vX = pvX;
+        vY = pvY;
+        aX = paX;
+        aY = paY;
+    }
+
+    public double getM() {
+        return m;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return
+                String.format("m=%010f", m) +
+                String.format(", x=%010f", x) +
+                String.format(", y=%010f", y) +
+                String.format(", vX=%010f", vX) +
+                String.format(", vY=%010f", vY) +
+                String.format(", aX=%010f", aX) +
+                String.format(", aY=%010f", aY);
+    }
 }
--- src/main/java/org/dubiner/Main.java
+++ src/main/java/org/dubiner/Main.java
@@ -1,7 +1,37 @@
 package org.dubiner;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main extends JFrame {
+
+    public Main() {
+        final Sim sim = new Sim();
+        add(sim);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Timer timer = sim.getTimer();
+                timer.stop();
+            }
+        });
+
+        setTitle("two body problem");
+        setSize(1000, 1000);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
     }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            Main main = new Main();
+            main.setVisible(true);
+        });
+    }
+
 }
